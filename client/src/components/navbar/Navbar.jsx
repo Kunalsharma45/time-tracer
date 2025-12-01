61b762764b1673af6e70114d93442264fc722de9
Kunal Sharma <kunalsharma1165@gmail.com>
2025-12-01 22:57:31 +0530

    This is the navbar edit

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
-          <div className="flex-shrink-0 font-bold text-xl text-blue-500 dark:text-blue-400">
+          <div className="shrink-0 font-bold text-xl text-blue-500 dark:text-blue-400">
             ProductivityTracker
           </div>
 
