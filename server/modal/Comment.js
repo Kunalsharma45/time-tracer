694f1563c7b1e9de49cdd3910e21e39d82d9a679
Kunal Sharma <kunalsharma1165@gmail.com>
2025-12-02 23:59:51 +0530

    Added the task comment modal

---
+++ server/modal/Comment.js
@@ -0,0 +1,61 @@
+// models/Comment.js
+import mongoose from 'mongoose';
+
+const commentSchema = new mongoose.Schema({
+  commentId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  likesCount: {
+    type: Number,
+    default: 0
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  taskId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task'
+  },
+  projectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Middleware
+commentSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+// Method to increment likes
+commentSchema.methods.incrementLikes = function() {
+  this.likesCount += 1;
+  return this.save();
+};
+
+// Method to decrement likes
+commentSchema.methods.decrementLikes = function() {
+  if (this.likesCount > 0) {
+    this.likesCount -= 1;
+  }
+  return this.save();
+};
+
+export default mongoose.model('Comment', commentSchema);
