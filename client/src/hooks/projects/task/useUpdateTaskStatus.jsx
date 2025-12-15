483e6cfe357eb23046a805b8555d478501cec334
Kunal Sharma <kunalsharma1165@gmail.com>
2025-12-16 01:37:43 +0530

    added the api to update the status of the task

---
+++ client/src/hooks/projects/task/useUpdateTaskStatus.jsx
@@ -0,0 +1,63 @@
+import { useState, useContext } from "react";
+import axios from "axios";
+import { ProjectContext } from "../../../context/project/ProjectContext";
+
+const useUpdateTaskStatus = () => {
+  const { setProject } = useContext(ProjectContext);
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const updateTaskStatus = async (taskId, status) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const res = await axios.put(
+        `${API_URL}/tasks/${taskId}/status`,
+        {
+          status,
+        },
+        {
+          headers: {
+            Authorization: token ? `Bearer ${token}` : "",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const updatedTask = res.data?.data?.task;
+      if (!updatedTask) return;
+
+      // accurate update
+      setProject((prev) => ({
+        ...prev,
+        tasks: prev.tasks.map((task) =>
+          task._id === taskId
+            ? {
+                ...task,
+                status: updatedTask.status,
+                completedAt: updatedTask.completedAt,
+              }
+            : task
+        ),
+      }));
+
+      return updatedTask;
+    } catch (err) {
+      console.error("Update status error:", err);
+      setError(err.response?.data?.message || "Failed to update status");
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    updateTaskStatus,
+    loading,
+    error,
+  };
+};
+
+export default useUpdateTaskStatus;
