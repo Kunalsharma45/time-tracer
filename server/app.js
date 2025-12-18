d628117e8db90a317d5117a119c5693c76537aa1
Kunal Sharma <kunalsharma1165@gmail.com>
2025-12-18 23:43:20 +0530

    feat: initialize Express server with core routes, middleware, CORS, and database connection.

--- server/app.js
+++ server/app.js
@@ -21,7 +21,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://time-analysis-and-productivity-q8tt.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
